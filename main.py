049a6f12dbea06f8a08cd14095b102dcec5b8239
Fow <99494790+Masat0r@users.noreply.github.com>
2022-12-18 20:25:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ import requests
 import random
 import string
 import time
+import os, string, random, codecs, time
+from random import randint
+from playsound import playsound
+import playsound
+import pygame
 
 print("""
 Atom Generator Nitro""")
 time.sleep(2)
-print("Generating Nitro Links")
+print("Generating Nitro Links  For better experience use HeadPhones")
 time.sleep(0.3)
 print("If you get some bug just dont mind\n")
 time.sleep(0.2)
+
+from pygame import mixer
+
+pygame.mixer.init()
+pygame.mixer.music.load('music2.mp3')
+pygame.mixer.music.play(999)
+
 
 num = int(input('Input How Many Codes to Generate and Check: '))
 
@@ -18,7 +30,7 @@ with open("Nitro Codes.txt", "w", encoding='utf-8') as file:
     print("Your nitro codes are being generated, be patient if you entered the high number!")
 
     start = time.time()
-
+    
     for i in range(num):
         code = "".join(random.choices(
             string.ascii_uppercase + string.digits + string.ascii_lowercase,
@@ -44,3 +56,4 @@ with open("Nitro Codes.txt") as file:
             print(f" Invalid | {nitro} ")
 
 input("\nYou have generated, Now press enter to close this, you'll get valid codes in Valid Codes.txt if you see its empty then you got no luck, generate 20 million codes for luck or else.")
+
